e88726e18383f6742da44fc17ebbba925166a40e
Priyanshu Joshi <71930422+priyanshu008@users.noreply.github.com>
2025-12-24 14:57:14 +0530

    Added solution - LeetHub

---
+++ 899451-count-elements-less-than-or-equal-to-k-in-a-sorted-rotated-array-GFG/899451-count-elements-less-than-or-equal-to-k-in-a-sorted-rotated-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countLessEqual(int[] arr, int x) {
+        // code here
+        int count = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] <= x)count++;
+        }
+        return count;
+    }
+}
